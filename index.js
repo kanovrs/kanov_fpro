f6e78eec29723da35068316ace0d83c813bc6a89
Kanov Rostyslav <kanovrs@gmail.com>
2023-08-20 08:15:43 +0300

    created a calculator

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-let numOne = +prompt(`Enter the first number`);
-let numTwo = +prompt(`Enter the second number`);
-
-alert(`${numOne} + ${numTwo} = ${numOne + numTwo}
-${numOne} - ${numTwo} = ${numOne - numTwo}
-${numOne} * ${numTwo} = ${numOne * numTwo}
-${numOne} / ${numTwo} = ${numOne / numTwo}`);
-
+let action = prompt ("Оберіть дію (add, sub, mult, div)" )
+let result;
+let num1 = +prompt("вкажіть перше число");
+let num2 = +prompt("вкажіть друге число");
+    switch (action) {
+      case 'add':
+        result = num1 + num2;
+        alert (`${num1} + ${num2} = ${result}`);
+        break;
+      case 'sub':
+        result = num1 - num2;
+        alert (`${num1} - ${num2} = ${result}`);
+        break;
+      case 'mult':
+        result = num1 * num2;
+        alert (`${num1} * ${num2} = ${result}`);
+        break;
+      case 'div':
+          result = num1 / num2;
+          alert (`${num1} / ${num2} = ${result}`);
+          break;
+    }
