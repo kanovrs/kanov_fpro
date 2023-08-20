c7ac933c63bbde1fa26431d62fa22dcc3bf92075
Kanov Rostyslav <kanovrs@gmail.com>
2023-08-20 08:11:44 +0300

    home-work3

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 let numOne = +prompt(`Enter the first number`);
 let numTwo = +prompt(`Enter the second number`);
 
-
 alert(`${numOne} + ${numTwo} = ${numOne + numTwo}
 ${numOne} - ${numTwo} = ${numOne - numTwo}
 ${numOne} * ${numTwo} = ${numOne * numTwo}
