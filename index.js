a6dbc8edea1ee98aabac228aa0ae8f47a90fb9ed
Kanov Rostyslav <kanovrs@gmail.com>
2023-08-22 08:13:31 +0300

    changed if to switch

--- index.js
+++ index.js
@@ -1,62 +1,17 @@
-let yearOfBirth = prompt("Вкажіть Ваш рік народження");
-let yearsOld, countryOrCity, champion;
+let numOrStr = prompt('input number or string');
+console.log(numOrStr);
 
-if (!yearOfBirth) {
-    yearsOld = "Шкода, що Ви не захотіли ввести Ваш рік народження!";
-    alert(yearsOld);
+switch(numOrStr) {
+case(numOrStr === null) :
+console.log('ви скасували');
+break;
+case(numOrStr.trim() === '') :
+console.log('Empty String');
+break;
+case(isNaN( +numOrStr )):
+console.log(' number is Ba_NaN');
+break;
+default:
+console.log('OK!');
 }
-else {
-    yearsOld = `Ваш вік ${2023 - yearOfBirth} років`;
-}
-
-let city = prompt("Вкажіть місто в якому ви проживаєте");
-
-if(!city) {
-    countryOrCity = "Шкода, що Ви не захотіли ввести свое мiсто!";
-    alert(countryOrCity);
-} else if(city) {
-    switch (city) {
-        case "Київ":
-            countryOrCity = "Ти живеш у столиці України!";
-            break;
-        case "Варшава":
-            countryOrCity = "Ти живеш у столиці Польщі!";
-            break;
-        case "Лондон":
-            countryOrCity = "Ти живеш у столиці Великобританії !";
-            break;
-        default:
-            countryOrCity = `Ти живеш у місті ${city}`;
-            break;
-    }
-} else {
-    countryOrCity = "Шкода, що Ви не захотіли ввести свое мiсто!";
-}
-
-let favoriteSport = prompt("Вкажіть Ваш улюбленный вид спорту");
-
-if(!favoriteSport){
-    champion = "Шкода, що Ви не захотіли ввести свой спорт!";
-} else {
-    switch (favoriteSport) {
-        case "Бокс":
-            champion = "Круто! Хочеш стати як Майк Тайсон?";
-            break;
-        case "Футбол":
-            champion = "Круто! Хочеш стати як Лионель Месси?";
-            break;
-        case "ММА":
-            champion = "Круто! Хочеш стати як Мак Греггор";
-            break;
-        default:
-            break;
-    }
-}
-
-alert (`${yearsOld} 
-${countryOrCity}
-${champion}`);
-
-
-
 
