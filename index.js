6840299782d7c451fbeff4916cc13364d05664be
Kanov Rostyslav <kanovrs@gmail.com>
2023-08-31 18:46:18 +0300

    use of abbreviation1

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 function removeElement(array, item) {
   const index = array.indexOf(item);
-  return (index !== -1) ? array.splice(index, 1) : null
-  // if (index !== -1) {
-  //     array.splice(index, 1);
-  // }
+  return (index !== -1) ? array.splice(index, 1) : null;
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7];
