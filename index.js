6254e410ee721a23518e3f14631136ff84d85171
Kanov Rostyslav <kanovrs@gmail.com>
2023-09-01 09:57:44 +0300

    function created

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-function generateKey(length, characters) {
-  let key = '';
-  let charactersLength = characters.length;
-
-  for (let i = 0; i < length; i++) {
-    let index = Math.floor(Math.random() * charactersLength);
-    key += characters[index];
-  }
-  return key;
+function recursiveExponentiation(num, degree) {
+  if (degree == 0) {
+    return 1;
+  } else {
+    if (degree == 1) {
+    return num;
+    }
+    else {
+      return num * recursiveExponentiation(num, degree - 1);
+    }
+  } 
 }
 
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-const key = generateKey(16, characters);
-console.log(key); // eg599gb60q926j8i
+console.log(recursiveExponentiation(3, 3));
