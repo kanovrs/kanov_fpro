288c621e222a3cf18b3fd13e0ddcbc1a8a7c82dd
Kanov Rostyslav <kanovrs@gmail.com>
2023-09-14 13:35:02 +0300

    added images

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-let input = document.querySelector('#input');
-let elementGhost = document.createElement('div');
+let imageName = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg'];
+let randomImage = Math.floor(Math.random() * imageName.length);
+let randomImageName = imageName[randomImage];
 
-elementGhost.style.border = 'thick double #32a1ce';
-elementGhost.style.padding = '20px';
-elementGhost.textContent = 'Div-ghost';
+let image = new Image();
+image.src = 'images/' + randomImageName;
 
-input.addEventListener('focus', function() {
-    document.body.append(elementGhost); 
-});
-
-input.addEventListener('blur', function () {
-    elementGhost.remove();
-});
+document.body.appendChild(image);
