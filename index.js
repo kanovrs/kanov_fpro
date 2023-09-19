9ccd47fb5b388a7cad85af501f37b20e359a9f39
Kanov Rostyslav <kanovrs@gmail.com>
2023-09-19 09:59:56 +0300

    created form

--- index.js
+++ index.js
@@ -1,29 +1,50 @@
-const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
-let currentImageIndex = 0;
+document.addEventListener(`DOMContentLoaded`, function () {
+  const form = document.querySelector(`#form-reg`);
+  const tableDate = document.querySelector(`#table-date`);
 
-const prevBtn = document.querySelector("#prevBtn");
-const nextBtn = document.querySelector("#nextBtn");
-const image = document.querySelector("#image");
+  form.addEventListener(`submit`, function (event) {
+    event.preventDefault();
 
-prevBtn.addEventListener("click", showPrevImage);
-nextBtn.addEventListener("click", showNextImage);
+    const name = form.elements.name.value;
+    const surName = form.elements.surname.value;
+    const dateOfBirthd = form.elements.dateOfBirth.value;
+    const gender = form.elements.gender.value;
+    const city = form.elements.city.value;
+    const address = form.elements.address.value;
+    const lang = [];
+    form.querySelectorAll('input[name="lang"]:checked').forEach(input => {
+      lang.push(input.value);
+    });
 
-function showNextImage() {
-  currentImageIndex++;
-  if (currentImageIndex >= images.length - 1) {
-    nextBtn.disabled = true;
-  }
-  prevBtn.disabled = false;
-  image.src = `./images/${images[currentImageIndex]}`;
-}
+    const table = document.createElement(`table`);
+    table.innerHTML = `
+       <caption>
+      Your data
+      </caption>
+      <tr>
+        <th>Name</th>
+        <th>Surname</th>
+        <th>Date of birth</th>
+        <th>Gender</th>
+        <th>City</th>
+        <th>Addres</th>
+        <th>Lang</th>
+      </tr>
+      <tr>
+        <td>${name}</td>
+        <td>${surName}</td>
+        <td>${dateOfBirthd}</td>
+        <td>${gender}</td>
+        <td>${city}</td>
+        <td>${address}</td>
+        <td>${lang.join(", ")}</td>
+      </tr>
+    `;
+    tableDate.innerHTML = "";
+    tableDate.appendChild(table);
 
-function showPrevImage() {
-  currentImageIndex--;
-  if (currentImageIndex <= 0) {
-    prevBtn.disabled = true;
-    currentImageIndex = 0;
-  } else {
-    nextBtn.disabled = false;
-  }
-  image.src = `./images/${images[currentImageIndex]}`;
-}
+    form.style.display = 'none';
+
+    tableDate.style.display = 'block';
+  });
+});
