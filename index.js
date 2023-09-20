25e865b8df8a4265a7b849251f1210e8e25ba9b4
Kanov Rostyslav <kanovrs@gmail.com>
2023-09-20 09:13:42 +0300

    hw 21

--- index.js
+++ index.js
@@ -1,50 +1,9 @@
-document.addEventListener(`DOMContentLoaded`, function () {
-  const form = document.querySelector(`#form-reg`);
-  const tableDate = document.querySelector(`#table-date`);
+let sum = 0;
 
-  form.addEventListener(`submit`, function (event) {
-    event.preventDefault();
+for (let i = 0; i < 3; i++) {
+  sum += +prompt(`Введіть ${i + 1}-е число`);
+}
 
-    const name = form.elements.name.value;
-    const surName = form.elements.surname.value;
-    const dateOfBirthd = form.elements.dateOfBirth.value;
-    const gender = form.elements.gender.value;
-    const city = form.elements.city.value;
-    const address = form.elements.address.value;
-    const lang = [];
-    form.querySelectorAll('input[name="lang"]:checked').forEach(input => {
-      lang.push(input.value);
-    });
+let result = sum / 3;
 
-    const table = document.createElement(`table`);
-    table.innerHTML = `
-       <caption>
-      Your data
-      </caption>
-      <tr>
-        <th>Name</th>
-        <th>Surname</th>
-        <th>Date of birth</th>
-        <th>Gender</th>
-        <th>City</th>
-        <th>Addres</th>
-        <th>Lang</th>
-      </tr>
-      <tr>
-        <td>${name}</td>
-        <td>${surName}</td>
-        <td>${dateOfBirthd}</td>
-        <td>${gender}</td>
-        <td>${city}</td>
-        <td>${address}</td>
-        <td>${lang.join(", ")}</td>
-      </tr>
-    `;
-    tableDate.innerHTML = "";
-    tableDate.appendChild(table);
-
-    form.style.display = 'none';
-
-    tableDate.style.display = 'block';
-  });
-});
+alert(`Середнє арифметичне цих чисел: ${result}`);
