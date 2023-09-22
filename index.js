decc557bd9cd9fc6244940adb7afa76c5198751c
Kanov Rostyslav <kanovrs@gmail.com>
2023-09-22 16:41:44 +0300

    complited hw24 p2

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+//Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
 let userText1 = prompt(``);
 let userText2 = prompt(``);
 let userText3 = prompt(``);
 
 alert(`${userText1} ${userText2} ${userText3}`);
+
+//Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл.
+let inputNumber = prompt("Введіть п'ятизначне число:");
+
+alert(`Результат: ${inputNumber.split("").join(" ")}`);
+
+
+
