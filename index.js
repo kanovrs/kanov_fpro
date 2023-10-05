e6febf38d1f5addf9f5618e175315d11ee629df9
Kanov Rostyslav <kanovrs@gmail.com>
2023-10-05 13:03:47 +0300

    created house

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-let link1 = document.querySelector('#link1');
-let link2 = document.querySelector('#link2');
-
-let protocolPattern = /^(https?:\/\/)/i;
-
-function redirectToSite(link) {
-    event.preventDefault();
-    let linkAtr = link.getAttribute('href');
-
-    if (!protocolPattern.test(linkAtr)) {
-        linkAtr = 'http://' + linkAtr;
-        link.setAttribute('href', linkAtr);
+class Person {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
     }
-
-    window.location.href = linkAtr;
 }
 
-link1.addEventListener('click', () => redirectToSite(link1, event));
-link2.addEventListener('click', () => redirectToSite(link2, event));
+class Apartment {
+    constructor() {
+        this.residents = [];
+    }
+
+    addResident(person) {
+        this.residents.push(person);
+    }
+}
+
+class House {
+    constructor(maxApartments) {
+        this.apartments = [];
+        this.maxApartments = maxApartments;
+    }
+
+    addApartment(apartment) {
+        if (this.apartments.length < this.maxApartments) {
+            this.apartments.push(apartment);
+        } else {
+            console.log("Максимальна кількість квартир у будинку вже досягнута.");
+        }
+    }
+}
+
+const person1 = new Person("Lucy", "woman");
+const person2 = new Person("Jon", "man");
+const person3 = new Person("Mike", "woman");
+
+const apartmentNomber1 = new Apartment();
+const apartmentNomber2 = new Apartment();
+const apartmentNomber3 = new Apartment();
+
+apartmentNomber1.addResident(person1);
+apartmentNomber2.addResident(person2);
+apartmentNomber3.addResident(person3);
+
+const house = new House(3); 
+
+house.addApartment(apartmentNomber1);
+house.addApartment(apartmentNomber2);
+house.addApartment(apartmentNomber3); 
