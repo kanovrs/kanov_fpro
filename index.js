d70c47fa143e0fb03588c17e13617474af131981
Kanov Rostyslav <kanovrs@gmail.com>
2023-10-12 14:41:53 +0300

    created class student

--- index.js
+++ index.js
@@ -1,48 +1,82 @@
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
+class Student {
+    constructor(firstName, lastName, birthYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+        this.grades = [];
+        this.attendance = new Array(25).fill(null);
     }
 
-    getInfo() {
-        console.log(`Ім'я:${this.name} Вік:${this.age}`)
-    }
-}
-
-class Car {
-    constructor(brand, model, year, number) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.number = number;
+    getAge() {
+        const currentYear = new Date().getFullYear();
+        return currentYear - this.birthYear;
     }
 
-    setOwner(owner) {
-        if (owner.age < 18) {
-            this.owner = owner;
+    addGrade(grade) {
+        this.grades.push(grade);
+    }
+
+    present() {
+        const index = this.attendance.findIndex(value => value === null);
+        if (index !== -1) {
+            this.attendance[index] = true;
         } else {
-            console.log("Власник автомобіля повинен бути старше 18 років.");
+            console.log('Не можна відвідати більше 25 заннять');
         }
     }
 
-    printCarInfo() {
-        console.log(`Марка: ${this.brand}, Модель: ${this.model}, Рік: ${this.year}, Номерний знак: ${this.number}`);
-        if (this.owner) {
-            console.log("Інформація про власника:");
-            this.owner.getInfo();
+    absent() {
+        const index = this.attendance.findIndex(value => value === null);
+        if (index !== -1) {
+            this.attendance[index] = true;
+        } else {
+            console.log('Не можна пропустити більше 25 заннять');
         }
     }
 
+    calculateAverageGrade() {
+        if (this.grades.length === 0) {
+            return 0;
+        }
+        const sum = this.grades.reduce((total, grade) => total + grade, 0);
+        return sum / this.grades.length;
+    }
+
+    summary() {
+        const averageGrade = this.calculateAverageGrade();
+        const attendanceRate = this.attendance.filter(value => value === true).length / 25;
+
+        if (averageGrade > 90 && attendanceRate > 0.9) {
+            return "Молодець!";
+        } else if (averageGrade > 90 || attendanceRate > 0.9) {
+            return "Добре, але можна краще";
+        } else {
+            return "Редиска!";
+        }
+    }
 }
 
-const person1 = new Person("Ann", 32);
-const person2 = new Person("Jon", 16);
+const student1 = new Student("Сергій", "Волков", 2005);
+const student2 = new Student("Аліна", "Костенко", 2006);
 
-const car1 = new Car("Toyota", "Camry", 2022, "AD777RE");
-const car2 = new Car("Mercedes", "clk-320", 2021, "KA044KA");
+student1.addGrade(99);
+student1.addGrade(89);
+student1.addGrade(94);
 
-car1.setOwner(person1); 
-car2.setOwner(person2);
+student2.addGrade(70);
+student2.addGrade(81);
+student2.addGrade(72);
 
-car1.printCarInfo(); 
-car2.printCarInfo();
+student1.present();
+student1.present();
+student1.absent();
+
+console.log(`${student1.firstName} ${student1.lastName}, вік: ${student1.getAge()} років`);
+console.log(`Середня оцінка: ${student1.calculateAverageGrade()}`);
+console.log(`Відвідуваність: ${student1.attendance.filter(value => value === true).length}/25`);
+console.log(student1.summary());
+
+console.log(`${student2.firstName} ${student2.lastName}, вік: ${student2.getAge()} років`);
+console.log(`Середня оцінка: ${student2.calculateAverageGrade()}`);
+console.log(`Відвідуваність: ${student2.attendance.filter(value => value === true).length}/25`);
+console.log(student2.summary());
